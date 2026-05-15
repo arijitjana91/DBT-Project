7ae606ebd7382c967a0c3fafd7e1f70fa43eb732
Jana <arijit.jana@accenture.com>
2026-05-15 11:36:53 +0530

    Snowflake

--- dbt_project_snow/models/bronze/bronze_store.sql
+++ dbt_project_snow/models/bronze/bronze_store.sql
@@ -1,5 +1,5 @@
 SELECT
     *
-{# FROM dbt_project.source.dim_store #}
-FROM
-    {{ source('source', 'dim_store') }}
+FROM DBT_PROJECT.SOURCE.dim_store
+{# FROM
+    {{ source('source', 'dim_store') }} #}
